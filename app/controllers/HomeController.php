5335ce7b256eaa7b529e23c989a93c4706001cdb
vince <vince@aspendigital.com>
2014-12-07 13:08:01 -0700

    Fixed navbar dashboard link

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -4,9 +4,12 @@ class HomeController extends BaseController {
 
 	public function home()
 	{
-		return View::make('landing_page')->with([
-			'page_name'	=>	'LANDING'
-		]);
+		if (Auth::check())
+			return Redirect::route('dashboard');
+		else
+			return View::make('landing_page')->with([
+				'page_name'	=>	'LANDING'
+			]);
 	}
 
 }

5f5aa37f09522dd72f97352af8dc9ed757c5b66a
Tyler <tychapman5@gmail.com>
2015-02-21 19:31:51 -0700

    Added Api keys to the communications
    Added api keys to both the servers

--- app/libraries/Communication.php
+++ app/libraries/Communication.php
@@ -38,8 +38,8 @@ class Communication
 
     private static function sendJsonToCoreService($mode, $json, $schedule_id)
     {
-        //$host = 'http://scheduling-core-service.herokuapp.com/api/' . $mode;
-        $host = 'localhost:8080/api/' . $mode;
+        $host = 'http://scheduling-core-service.herokuapp.com/api/' . $mode;
+        //$host = 'localhost:8080/api/' . $mode;
 
 		//will need to set up
 		$curl = curl_init($host);
@@ -67,6 +67,8 @@ class Communication
     	$jsonBuilder = [];
     	$eventsBuilder = [];
     	$roomBuilder = [];
+
+        $jsonBuilder['APIKey'] = "1bb0ea87-d786-4300-903d-e3aa4e3ac670";
 
     	foreach ($events as $event) 
     	{

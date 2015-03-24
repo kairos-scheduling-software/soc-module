8968b5be2fa9c6f50f3e632678f43891c9912552
natecr <llatikcuf@gmail.com>
2015-03-24 19:07:59 +0000

    Fixed heatmaps

--- public/assets/vis/js/grid.js
+++ public/assets/vis/js/grid.js
@@ -22,3 +22,31 @@
     dashboard1.render();
 
 }());
+
+var visDays = ["U", "M", "T", "W", "H", "F", "S"];
+
+function visGetSchedData(sched_name) {
+    /*
+    $.ajax({
+        dataType: "json",
+        url: 'vis/8/0',
+        success: function (data) {
+            var newDays;
+            $.each(data, function (i, d) {
+                newDays = [];
+                var pd = jQuery.parseJSON(d.days);
+                for (var j = 0; j < pd.length; j++) {
+                    newDays[j] = visDays[pd[j]];
+                }
+                data[i].days = newDays;
+            });
+        }
+    });
+    */
+    console.log(sched_name);
+    if (sched_name == "None") {
+        return [];
+    }
+
+    return vis_data[sched_name];
+}

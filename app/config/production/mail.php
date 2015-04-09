751fffcdaef4b940985f390830ab726d043745a8
vince <vince@aspendigital.com>
2015-04-09 12:23:55 -0600

    Troubleshooting registration emails

--- app/config/production/mail.php
+++ app/config/production/mail.php
@@ -93,7 +93,7 @@ return array(
 	|
 	*/
 
-	'password' => 'kairos2015',
+	'password' => getenv('email_password'),
 
 	/*
 	|--------------------------------------------------------------------------

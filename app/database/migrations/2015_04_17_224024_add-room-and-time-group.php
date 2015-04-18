a677b51d0ec2f60985c20fdf94e22a4cfb359f79
Vinh Dang <dtt.vinh@gmail.com>
2015-04-18 14:12:48 -0600

    Fixed db migration.

---
+++ app/database/migrations/2015_04_17_224024_add-room-and-time-group.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddRoomAndTimeGroup extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('room_groups', function($table)
+		{
+			$table->string('name', 10)->unique();
+			$table->string('description', 50);
+		});
+		
+		Schema::create('room_mappings', function($table)
+		{
+			$table->string('name', 10);
+			$table->int('rid')->unsigned;
+			$table->unique(array('name', 'rid'));
+			
+			$table->foreign('name')->references('name')->on('room_groups')->onDelete('cascade');
+			$table->foreign('rid')->references('id')->on('rooms')->onDelete('cascade');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('room_groups');
+		Schema::drop('room_mappings');
+	}
+
+}

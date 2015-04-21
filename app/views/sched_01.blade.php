f88af163d87ef11450a7e9fe22ee1faeb9daa4d8
natecr <llatikcuf@gmail.com>
2015-04-21 18:46:56 +0000

    Bug fixes

--- app/views/sched_01.blade.php
+++ app/views/sched_01.blade.php
@@ -10,21 +10,21 @@
                 <div class="title">Visualizations</div>
                 @if(Auth::check())
                 <ul id="vis-menu-list" class="list">
-                    <li class="list-item">
+                    <li class="list-item" title="Total class counts by year/semester and class counts by professor based on a user selected year/semester">
                         <div class="item-content" data-href="#dashboard/1">
                             <i class="glyphicon glyphicon-align-right gly-rotate-90"></i>
                             <h3>Class Counts</h3>
                             <p>Year/Semester</p>
                         </div>
                     </li>
-                    <li class="list-item">
+                    <li class="list-item" title="Class counts per hour/day based on a user selected schedule">
                         <div class="item-content" data-href="#dashboard/2">
                             <i class="glyphicon glyphicon-fire"></i>
                             <h3>Heat Maps</h3>
                             <p>Year/Semester</p>
                         </div>
                     </li>
-                    <li class="list-item">
+                    <li class="list-item" title="Visualization of a schedule and schedule diff checker">
                         <div class="item-content" data-href="#dashboard/3">
                             <i class=" glyphicon glyphicon-align-center gly-rotate-90"></i>
                             <h3>Schedule Explorer</h3>

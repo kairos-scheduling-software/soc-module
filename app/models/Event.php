8ec585530262544a5087a049123efefaf14dc9de
Vinh Dang <dtt.vinh@gmail.com>
2015-04-26 00:11:19 -0600

    Added fields to the event model.

--- app/models/Event.php
+++ app/models/Event.php
@@ -4,7 +4,7 @@ namespace models;
 class Event extends \Eloquent 
 {
 	protected $table = 'events';
-	protected $fillable = array('name','professor', 'schedule_id', 'room_id', 'class_type', 'title', 'etime_id');
+	protected $fillable = array('name','professor', 'schedule_id', 'room_id', 'room_group_id', 'is_rm_final', 'class_type', 'title', 'etime_id');
 	protected $touches = array('schedule');
 
 	public function constraints()
